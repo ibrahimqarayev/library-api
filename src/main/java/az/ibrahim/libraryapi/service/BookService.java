8953b3f51c45fbacbbab394a54dfe8e4fcf26caa
ibrahimqarayev <qarayevibrahim7@mail.ru>
2026-07-21 23:31:16 +0400

    chore: remove unused imports

--- src/main/java/az/ibrahim/libraryapi/service/BookService.java
+++ src/main/java/az/ibrahim/libraryapi/service/BookService.java
@@ -19,9 +19,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 @Service
 @RequiredArgsConstructor
 public class BookService {
