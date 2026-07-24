cb76d27e1b8fbbb18210a54fc5dfedba77938751
ibrahimqarayev <qarayevibrahim7@mail.ru>
2026-07-24 16:55:58 +0400

    feat: add role-based access control for users and admins

--- src/main/java/az/ibrahim/libraryapi/config/SecurityConfig.java
+++ src/main/java/az/ibrahim/libraryapi/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import az.ibrahim.libraryapi.security.jwt.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -24,17 +25,45 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
                 .authorizeHttpRequests(auth -> auth
+
+                        // Public
                         .requestMatchers(
                                 "/api/v1/auth/**",
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
                                 "/v3/api-docs/**"
                         ).permitAll()
+
+                        // USER + ADMIN
+                        .requestMatchers(HttpMethod.GET, "/api/v1/books/**")
+                        .hasAnyRole("USER", "ADMIN")
+
+                        .requestMatchers(HttpMethod.GET, "/api/v1/authors/**")
+                        .hasAnyRole("USER", "ADMIN")
+
+                        .requestMatchers(HttpMethod.GET, "/api/v1/members/**")
+                        .hasAnyRole("USER", "ADMIN")
+
+                        // ADMIN only
+                        .requestMatchers("/api/v1/books/**")
+                        .hasRole("ADMIN")
+
+                        .requestMatchers("/api/v1/authors/**")
+                        .hasRole("ADMIN")
+
+                        .requestMatchers("/api/v1/members/**")
+                        .hasRole("ADMIN")
+
                         .anyRequest().authenticated()
                 )
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(
+                        jwtAuthenticationFilter,
+                        UsernamePasswordAuthenticationFilter.class
+                );
 
         return http.build();
     }
