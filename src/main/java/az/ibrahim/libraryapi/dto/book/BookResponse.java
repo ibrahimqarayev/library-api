8f207c536b25df8566550a962dbbd6443ef72226
ibrahimqarayev <qarayevibrahim7@mail.ru>
2026-07-30 15:11:02 +0400

    fix: add setter to BookResponse for MapStruct mapping

--- src/main/java/az/ibrahim/libraryapi/dto/book/BookResponse.java
+++ src/main/java/az/ibrahim/libraryapi/dto/book/BookResponse.java
@@ -3,8 +3,10 @@ package az.ibrahim.libraryapi.dto.book;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class BookResponse {
