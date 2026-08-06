d6fc91dafec90b9c6431512d082d65f7f5440d2b
ibrahimqarayev <qarayevibrahim7@mail.ru>
2026-08-06 23:35:18 +0400

    feat: add derived query methods and JPQL queries for book filtering

--- src/main/java/az/ibrahim/libraryapi/repository/BookRepository.java
+++ src/main/java/az/ibrahim/libraryapi/repository/BookRepository.java
@@ -2,6 +2,42 @@ package az.ibrahim.libraryapi.repository;
 
 import az.ibrahim.libraryapi.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface BookRepository extends JpaRepository<Book,Long> {
+import java.util.List;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+    List<Book> findByTitleContainingIgnoreCase(String title);
+
+    List<Book> findByPublicationYearBetween(Integer startYear, Integer endYear);
+
+    @Query("""
+            SELECT b
+            FROM Book b
+            JOIN b.author a
+            WHERE LOWER(a.name) LIKE LOWER(CONCAT('%', :authorName, '%'))
+            """)
+    List<Book> findByAuthorName(@Param("authorName") String authorName);
+
+    @Query("""
+            SELECT b
+            FROM Book b
+            JOIN b.categories c
+            WHERE LOWER(c.name) = LOWER(:categoryName)
+            """)
+    List<Book> findByCategoryName(@Param("categoryName") String categoryName);
+
+    @Query("""
+            SELECT DISTINCT b
+            FROM Book b
+            JOIN b.author a
+            JOIN b.categories c
+            WHERE LOWER(a.name) LIKE LOWER(CONCAT('%', :authorName, '%'))
+              AND LOWER(c.name) = LOWER(:categoryName)
+            """)
+    List<Book> findByAuthorAndCategory(
+            @Param("authorName") String authorName,
+            @Param("categoryName") String categoryName
+    );
 }
