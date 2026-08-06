d2db2584604262ee7d0831f53f43d3ad49bd0ce8
ibrahimqarayev <qarayevibrahim7@mail.ru>
2026-08-06 23:58:57 +0400

    perf: resolve N+1 query problem with EntityGraph

--- src/main/java/az/ibrahim/libraryapi/repository/BookRepository.java
+++ src/main/java/az/ibrahim/libraryapi/repository/BookRepository.java
@@ -1,14 +1,27 @@
 package az.ibrahim.libraryapi.repository;
 
 import az.ibrahim.libraryapi.entity.Book;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
+
+    @EntityGraph(attributePaths = {"author", "categories"})
+    List<Book> findAll();
+
+    @EntityGraph(attributePaths = {"author", "categories"})
+    Optional<Book> findById(Long id);
+
+    @EntityGraph(attributePaths = {"author", "categories"})
+    List<Book> findAll(Specification<Book> specification);
+
     List<Book> findByTitleContainingIgnoreCase(String title);
 
     List<Book> findByPublicationYearBetween(Integer startYear, Integer endYear);
