5a1ebe8d1ae5d0306cf95c6ac4874f3edeb83266
ibrahimqarayev <qarayevibrahim7@mail.ru>
2026-08-12 23:18:46 +0400

    feat: configure async task executor

---
+++ src/main/java/az/ibrahim/libraryapi/config/AsyncConfig.java
@@ -0,0 +1,31 @@
+package az.ibrahim.libraryapi.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+@EnableConfigurationProperties(AsyncProperties.class)
+@RequiredArgsConstructor
+public class AsyncConfig {
+
+    private final AsyncProperties properties;
+
+    @Bean(name = "taskExecutor")
+    public Executor taskExecutor() {
+
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(properties.getCorePoolSize());
+        executor.setMaxPoolSize(properties.getMaxPoolSize());
+        executor.setQueueCapacity(properties.getQueueCapacity());
+        executor.setThreadNamePrefix(properties.getThreadNamePrefix());
+        executor.initialize();
+        return executor;
+    }
+}
