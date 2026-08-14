ad469858524dd968a563e3e065a83b041cad2254
ibrahimqarayev <qarayevibrahim7@mail.ru>
2026-08-14 17:42:03 +0400

    feat: add file listing to storage service

--- src/main/java/az/ibrahim/libraryapi/service/inter/FileStorageService.java
+++ src/main/java/az/ibrahim/libraryapi/service/inter/FileStorageService.java
@@ -3,6 +3,8 @@ package az.ibrahim.libraryapi.service.inter;
 import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 public interface FileStorageService {
 
     String store(MultipartFile file, String fileName);
@@ -10,4 +12,6 @@ public interface FileStorageService {
     Resource load(String filePath);
 
     void delete(String filePath);
+
+    List<String> listFiles();
 }
