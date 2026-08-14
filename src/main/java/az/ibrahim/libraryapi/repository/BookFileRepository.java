3a6feaae0753c6c6762cb95cf05faa03e9aed647
ibrahimqarayev <qarayevibrahim7@mail.ru>
2026-08-14 17:41:32 +0400

    feat: add query for stored book file paths

--- src/main/java/az/ibrahim/libraryapi/repository/BookFileRepository.java
+++ src/main/java/az/ibrahim/libraryapi/repository/BookFileRepository.java
@@ -2,10 +2,15 @@ package az.ibrahim.libraryapi.repository;
 
 import az.ibrahim.libraryapi.entity.BookFile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BookFileRepository extends JpaRepository<BookFile, Long> {
 
     Optional<BookFile> findByBookId(Long bookId);
+
+    @Query("select b.filePath from BookFile b")
+    List<String> findAllFilePaths();
 }
